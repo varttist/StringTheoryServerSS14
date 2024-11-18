cdd21acd64b940f0b731f34e5d16ec11b19b41b5
Welldzhik <163353146+Welldzhik@users.noreply.github.com>
2024-11-18 05:01:27 +0300

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,22 @@
-#!/bin/sh
-dotnet run --project Content.Server
-read -p "Press enter to continue"
+#!/bin/bash
+
+# Переменная для хранения текущей версии
+CURRENT_HASH=$(git rev-parse HEAD)
+
+# Обновление репозитория
+git pull
+git submodule update --init --recursive
+
+# Проверка текущей версии с последней версией
+NEW_HASH=$(git rev-parse HEAD)
+
+# Если хеши отличаются, пересобираем сервер
+if [ "$CURRENT_HASH" != "$NEW_HASH" ]; then
+    echo "Найдены обновления, пересборка сервера..."
+    dotnet build -c release
+else
+    echo "Обновлений нет, запуск сервера..."
+fi
+
+# Запуск сервера
+dotnet run --project Content.Server --configuration Release
