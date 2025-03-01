7f7df3997ba8222213873fe0ce0f19524222df3d
Welldzhik <163353146+Welldzhik@users.noreply.github.com>
2025-03-01 16:46:05 +0300

    Update runserver.sh
    Изменения для обновления хеша

--- runserver.sh
+++ runserver.sh
@@ -14,7 +14,7 @@ NEW_HASH=$(git rev-parse HEAD)
 if [ "$CURRENT_HASH" != "$NEW_HASH" ]; then
     echo "Найдены обновления, пересборка сервера..."
     dotnet build -c release
-   cp Resources/ConfigPresets/_ST14/st14_server.toml bin/Content.Server/server_config.toml
+    cp Resources/ConfigPresets/_ST14/st14_server.toml bin/Content.Server/server_config.toml
 else
     echo "Обновлений нет, запуск сервера..."
 fi
