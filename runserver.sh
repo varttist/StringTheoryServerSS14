98f15f5f58f635a445dcbe546f8a46d483728c71
Welldzhik <163353146+Welldzhik@users.noreply.github.com>
2025-03-28 23:36:55 +0300

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # Переменная для хранения текущей версии
-CURRENT_HASH=$(git rev-parse HEAD)
+# CURRENT_HASH=$(git rev-parse HEAD)
 
 # Обновление репозитория
 git pull
 git submodule update --init --recursive
+dotnet build -c release
+сp Resources/ConfigPresets/_ST14/st14_server.toml bin/Content.Server/server_config.toml
 
 # Проверка текущей версии с последней версией
-NEW_HASH=$(git rev-parse HEAD)
+# NEW_HASH=$(git rev-parse HEAD)
 
 # Если хеши отличаются, пересобираем сервер
-if [ "$CURRENT_HASH" != "$NEW_HASH" ]; then
-    echo "Найдены обновления, пересборка сервера..."
-    dotnet build
-    cp Resources/ConfigPresets/_ST14/st14_server.toml bin/Content.Server/server_config.toml
-else
-    echo "Обновлений нет, запуск сервера..."
-fi
+# if [ "$CURRENT_HASH" != "$NEW_HASH" ]; then
+#     echo "Найдены обновления, пересборка сервера..."
+#     dotnet build -c release
+#     cp Resources/ConfigPresets/_ST14/st14_server.toml bin/Content.Server/server_config.toml
+# else
+#     echo "Обновлений нет, запуск сервера..."
+# fi
 
 # Запуск сервера
 dotnet run --project Content.Server --configuration Release
